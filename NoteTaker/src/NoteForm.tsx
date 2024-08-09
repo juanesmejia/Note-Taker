f1120e385bfceaa41673e6da12caf3c597e0d4a1
Juan Mejia <juanesmejia1220@gmail.com>
2024-08-09 10:54:23 -0400

    created buttons on home screen

--- NoteTaker/src/NoteForm.tsx
+++ NoteTaker/src/NoteForm.tsx
@@ -1,6 +1,6 @@
 import { FormEvent, useRef, useState } from "react";
 import { Button, Col, Form, Row, Stack } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import CreatableReactSelect from "react-select/creatable";
 import { NoteData, Tag } from "./App";
 import { v4 as uuidV4 } from "uuid";
@@ -15,6 +15,8 @@ export function NoteForm({ onSubmit, onAddTag, availibleTags }: NoteFormProps) {
   const titleRef = useRef<HTMLInputElement>(null);
   const markdownRef = useRef<HTMLTextAreaElement>(null);
   const [selectTags, setTags] = useState<Tag[]>([]);
+  const navigate = useNavigate();
+
   function handlerSubmit(e: FormEvent) {
     e.preventDefault();
     onSubmit({
@@ -22,6 +24,7 @@ export function NoteForm({ onSubmit, onAddTag, availibleTags }: NoteFormProps) {
       markdown: markdownRef.current!.value,
       tags: selectTags,
     });
+    navigate("..");
   }
 
   return (
